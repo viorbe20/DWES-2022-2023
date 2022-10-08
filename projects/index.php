4102e0ec78bb2ee3a197884494066f73a66aacac
Virginia Ordoño Bernier <viorbe20@gmail.com>
2022-10-08 13:46:02 +0200

    feat: feat: verbs test shows main view

---
+++ projects/index.php
@@ -0,0 +1,39 @@
+<?php
+require("../require/view_home.php");
+
+$ejercicios = array(
+    array('id' => 1, 'titulo' => 'Ejercicio 1', 'descripcion' => 'Verbos irregulares.', 'enlace' => 'verbos//index.php', 'github' => 'https://github.com/viorbe20/DWES-2022-2023/tree/main/projects/verbs/index.php'),
+)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' href='../css/style_units.css' />
+    <title>Índice Proyectos</title>
+</head>
+
+<body>
+    <h1>Ejercicios Desarrollo Web Entorno Servidor (2022/2023)</h1>
+    <h3>Ud2. Sintaxis general</h3>
+    <main>
+        <?php
+        foreach ($ejercicios as $key => $value) {
+            echo '<article id="art' . $value['id'] . '">
+        <div class="titulo">' . $value['titulo'] . '</div>
+        <div class="descripcion">' . $value['descripcion'] . '</div>
+        <div class="enlaces">
+        <a target="_blank" href="' . $value['enlace'] . '">Ejercicio</a>
+        <a target="_blank" href="' . $value['github'] . '">Código</a>
+        </div>
+        </article>';
+        }
+        ?>
+    </main>
+</body>
+
+</html>
--- projects/verbos/index.php
+++ projects/verbos/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * App que muestra un test sobre la lista de verbos irregulares en inglés.
  * Se pueden elegir la cantidad de verbos que se pueden mostrar.
@@ -10,24 +11,81 @@
 require("../../require/view_home.php");
 // Incluimos el fichero con la lista de verbos
 include 'irregular_verbs.php';
+
+//Variables
+$processTestType = False;
+$processResult = False;
+$selectedTestType = array();
+
+// Tipo de test seleccionado
+if (isset($_POST['submit_test_type'])) {
+    $processTestType = True;
+    array_push($selectedTestType, $_POST['verbs_num'], $_POST['level']);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
-<meta charset='UTF-8'>
-<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-<title>Irregular verbs</title>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Irregular verbs</title>
 </head>
+<style>
+    <?php
+    echo file_get_contents("css/style.css");
+    ?>
+</style>
+
 <body>
-<main>
+    <main>
+        <h1>Test Verbos Irregulares</h1>
+        <hr>
+        <?php
+        if (!$processTestType && !$processResult) {
+        ?>
+        <form action='index.php' method='post'>
+            <div id="option1">
+                <label for='verbs_num'>Número de verbos a mostrar:
+                    <input type='number' name='verbs_num' min='1' max='10' value='5'>
+                </label>
+                <br>
+                <label for='level'>Dificultad:
+                    <select name='level'>
+                        <option value='alta'>Alta</option>
+                        <option value='media'>Media</option>
+                        <option value='baja'>Baja</option>
+                    </select>
+                </label>
+                <br>
+                <input type="submit" name='submit_test_type' value='Enviar'>
+            </div>
+        </form>
+        <?php
+        } else if ($processTestType && !$processResult){
+            ?>
+            <div id="test_description">
+                <div>
+                    <label>Nivel de dificultad: 
+                        <span><?php echo $selectedTestType[1]; ?></span>
+                    </label>
+                </div>
+                <div>
+                    <label>Números de verbos: 
+                        <span><?php echo $selectedTestType[0]; ?></span>
+                    </label>
+                </div>
+            </div>
+        <form action='index.php' method='post'>
+        </form>
+        <?php
+        }
 
-<?php
-
-?>
-</main>
+        ?>
+    </main>
 </body>
+
 </html>
-
-
