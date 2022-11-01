c46bf0205ab596067f9a331abaf22399bca03a9e
Virginia Ordoño Bernier <viorbe20@gmail.com>
2022-11-01 08:11:57 +0100

    company info can delete employee section

--- projects/fct_management/view/js/main.js
+++ projects/fct_management/view/js/main.js
@@ -1,7 +1,20 @@
-// Add employee button click event from 'alta empresa'
 $(document).ready(function () {
-
+    // Add employee button click event from 'alta empresa'
+    
     $("#add_employee_section").click(function () {
-        $("#new_employee_data > section:nth-child(2)").clone().css("display", "flex").appendTo("#new_employee_data");
+        $("#new_employee_data > section:nth-child(2)")
+        .clone()
+        .css("display", "flex")
+        .appendTo("#new_employee_data");
+        
+        //Delete selected employee section
+        let cancelSpan = $("#new_employee_data > section > span")
+        console.log(cancelSpan);
+        cancelSpan.each(function () {
+            $(this).click(function () {
+                $(this).parent().remove()
+            })
+        })
     });
+
 });
