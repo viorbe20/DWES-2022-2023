b9b84ed5ac366da87a291413ee778ef916993c11
Virginia Ordoño Bernier <viorbe20@gmail.com>
2022-12-05 18:08:11 +0100

    show employees deleted modal but not modif on db

--- projects/fct/assets/js/company_employees.js
+++ projects/fct/assets/js/company_employees.js
@@ -1,5 +1,11 @@
+function deleteEmployee(employeeName) {
+    $('#delete_message').append('Se ha eliminado el empleado ' + employeeName);
+    $("#modal_delete_employee").css("display", "block");
+}
+
+
 $(document).ready(function () {
-console.log('on company_employees.js');
+    console.log('on company_employees.js');
 
     /**
      * Searh employee box
@@ -9,5 +15,22 @@ console.log('on company_employees.js');
         $("#table_body_employees tr").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
+    });
+
+    /**
+     * Modal delete employee
+     * Click on close button, close modal.
+     */
+    $("#modal_delete_employee #btn_modal_exit").click(function () {
+        $("#modal_delete_employee").css("display", "none");
+        window.location.reload();
+    });
+
+    /**
+    * Modal delete employee
+    * Click on X button
+    */
+    $("#modal_delete_employee #span_modal_exit").click(function () {
+        $("#modal_delete_employee").css("display", "none");
     });
 });
--- projects/fct/view/company_employees.php
+++ projects/fct/view/company_employees.php
@@ -46,7 +46,7 @@
                         echo "<td>" . $employee['emp_name'] . "</td>";
                         echo "<td>" . $employee['emp_job'] . "</td>";
                         echo "<td>";
-                        echo "<a href='http://localhost/dwes/projects/fct/employee/" . $employee['emp_id'] . "'><span class=\"material-symbols-outlined\">delete</span>";
+                        echo "<a href='#' target='_self' onclick='deleteEmployee(". $employee['emp_id'] .")'><span class=\"material-symbols-outlined\">delete</span>";
                         echo "<a href='http://localhost/dwes/projects/fct/employee/" . $employee['emp_id'] . "'><span class=\"material-symbols-outlined\">edit</span></td>";
                         echo "</tr>";
                     }
@@ -58,7 +58,24 @@
 
     </div>
 
+    <!-- Modal Employee Deleted -->
+    <div class="modal" tabindex="-1" role="dialog" id="modal_delete_employee">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Empresa</h5>
+                    <button type="text" class="btn btn-secondary text-lg border-rounded" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="text-ligth" id="span_modal_exit">X</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="delete_message">
+                </div>
+                <div class="modal-footer">
+                    <button id="btn_modal_exit" name="btn_modal_close" type="button" class="btn btn-secondary" data-dismiss="modal">Salir</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
-
