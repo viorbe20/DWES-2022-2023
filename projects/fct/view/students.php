2646d508432d1d3753b7d6bbdd758b4c392dd9e7
Virginia Ordoño Bernier <viorbe20@gmail.com>
2023-01-16 11:04:47 +0100

    styling student box search

--- projects/fct/view/students.php
+++ projects/fct/view/students.php
@@ -11,32 +11,35 @@
     <link rel='stylesheet' href="http://localhost/dwes/projects/fct/assets/css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="http://localhost/dwes/projects/fct/assets/js/employees.js"></script>
-    <title>FCT Companies</title>
+    <!-- <script src="http://localhost/dwes/projects/fct/assets/js/employees.js"></script> -->
+    <title>FCT Students</title>
 </head>
 
 <body>
     <?php
     require_once '../view/require/header.php';
     ?>
-    <!--Content-->
     <div class="container d-flex-column justify-content-center mt-5">
-        
-    <!--Search box company-->
-        <form method="post" id="form_search_company" class="d-flex m-5" role="search">
-            <input name="input_search_company" id="input_search_company" class="form-control me-5" type="text" placeholder="Nombre de la empresa">
-            <button name="btn_search_company" id="btn_search_company" class="btn btn-primary mx-1 w-25" type="submit">Buscar</button>
-            <a href="<?php echo DIRBASEURL;?>/home/create_company" class="btn btn-success mx-1">Crear Empresa</a>
+        <form method="post" id="form_search_company" class="d-flex align-items-center justify-content-center" role="search">
+            <div class='d-flex w-75'>
+                <input name="input_search_company" id="input_search_company" class="form-control m-" type="text" placeholder="Nombre del alumno">
+                <button type="submit" class="btn btn-primary">
+                    <span class="material-symbols-outlined">
+                        search
+                    </span>
+                </button>
+            </div>
+            <!-- <button name="btn_search_company" id="btn_search_company" class="btn btn-primary mx-1 w-25" type="submit">Buscar</button> -->
+            <a href="" class="btn btn-success mx-1">Cargar archivo</a>
         </form>
-        
-        
+
+
         <table id="table-companies" class="table text-center mt-5">
             <thead>
                 <tr>
-                    <th scope="col">Logo</th>
                     <th scope="col">Nombre</th>
-                    <th scope="col">Teléfono</th>
-                    <th scope="col">Empleados</th>
+                    <th scope="col">Primer Apellido</th>
+                    <th scope="col">Segundo Apellido</th>
                     <th scope="col">Opciones</th>
                 </tr>
             </thead>
